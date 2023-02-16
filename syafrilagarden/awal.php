269fb1ee2abce9eac160834dea2624c5d0afd50e
Agha Syafrila Myzantina <89903725+aghasyafrila@users.noreply.github.com>
2023-02-16 08:08:57 +0700

    Add files via upload

---
+++ syafrilagarden/awal.php
@@ -0,0 +1,15 @@
+<div class="main-content" style="background-image:url('images/monstera.jpg');background-repeat: no-repeat;
+  background-position: center;"> 
+<div class="section__content section__content--p30">
+                    <div class="container-fluid">
+						<div class="row">
+                            <div class="col-md-12">
+                                <div>
+                <p><h1>Selamat Datang</h1></p>
+								<p><h2>Silahkan pantau tanaman anda agar senantiasa sehat nan cantik!!</h2><p>
+                                </div>
+                            </div>
+						</div>
+					</div>
+				</div>
+			</div>
